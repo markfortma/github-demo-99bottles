088bb4651d4f5c3f16d0d01a7f361f85f647e54c
Matt Markfort <matthew.markfort@gmail.com>
2023-02-11 15:01:11 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+* Git Demo using 99bottles in Java
+
+** Description
+
+The purpose of this repository is to create an automated demonstration
+of git using "99 Bottles of Beer on the Wall" (starting at 4 bottles left).
+
+The Java source files will be staged at each change, rather than diffs
+in order for future development using Windows command-prompt/PowerShell.
+
+Initially, this will be developed using BASH.
+
+** Authors
+
+Matt Markfort
+
---
+++ java/1_Round.java
@@ -0,0 +1,23 @@
+/*
+ * author   Matt Markfort
+ * date     2023-02-11
+ *
+ * description
+ *          The number preceding this file name is to ensure that
+ * the correct order of changes to the commit occur.
+ *
+ *          This will perform "99 Bottles of Beer on the Wall" 
+ * (starting at 4, for brevity).
+ */
+
+public class Round {
+    // The countdown will start here and a copy will decrement for the
+    // performance
+    private final int START = 4;
+
+    public static void main(String[] args){
+	// A serial countdown as per the performance
+	for(int i = START; i >= 0; i--){
+	    
+    }
+}
